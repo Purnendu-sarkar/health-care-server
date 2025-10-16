176b0e494db58177244456357cac1febf182cabb
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-10-16 07:12:26 +0600

    Implement createPatient functionality with Prisma transaction, password hashing, and structured response

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
+import { UserService } from "./user.service";
+import sendResponse from "../../shared/sendResponse";
 
 const createPatient = catchAsync(async (req: Request, res: Response) => {
-    console.log("Patient", req.body)
+    const result = await UserService.createPatient(req.body);
+    console.log(result)
+
+    sendResponse(res, {
+        statusCode: 201,
+        success: true,
+        message: "Patient created successfully!✅",
+        data: result
+    })
 })
 
 export const UserController = {
