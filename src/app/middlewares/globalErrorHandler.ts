8e725f35a9102b2f77b46b0c9d125cfb84afb429
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-10-22 17:46:09 +0600

    feat: add detailed Prisma error handling for unique, auth, and foreign key constraint errors

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express"
 import httpStatus from "http-status"
 
@@ -7,6 +8,35 @@ const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFun
     let success = false;
     let message = err.message || "Something went wrong!";
     let error = err;
+
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === "P2002") {
+            // Unique constraint failed
+            message = "Duplicate value detected. This record already exists.";
+            error = err.meta;
+        }
+
+        if (err.code === "P1000") {
+            // Database authentication failed
+            message =
+                "Database authentication failed. Please check your database credentials or connection settings.";
+            error = err.meta;
+        }
+
+        if (err.code === "P2003") {
+            // Foreign key constraint failed
+            message =
+                "Operation failed due to an invalid or missing related record. Please check your foreign key references.";
+            error = err.meta;
+        }
+
+        if (err.code === "P2025") {
+            // Record not found
+            message =
+                `Requested ${err.meta?.modelName || "record"} not found. Please verify the provided ID or query parameters.`;
+            error = err.meta;
+        }
+    }
 
     res.status(statusCode).json({
         success,
