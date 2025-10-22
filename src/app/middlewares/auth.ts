fd90fef2e6e9b187c1c6dfafdd821e002467be33
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-10-22 18:56:20 +0600

    Implementing ApiError Handling

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,6 +1,8 @@
+import httpStatus from 'http-status';
 import { NextFunction, Request, Response } from "express"
 import { jwtHelper } from "../helper/jwtHelper";
 import config from "../config";
+import ApiError from "../errors/ApiError";
 
 const auth = (...roles: string[]) => {
     return async (req: Request & { user?: any }, res: Response, next: NextFunction) => {
@@ -8,7 +10,7 @@ const auth = (...roles: string[]) => {
             const token = req.cookies.accessToken;
 
             if (!token) {
-                throw new Error("You are not authorized!")
+                throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized!")
             }
 
             const verifyUser = jwtHelper.verifyToken(token, config.jwt_access_secret);
@@ -16,7 +18,7 @@ const auth = (...roles: string[]) => {
             req.user = verifyUser;
 
             if (roles.length && !roles.includes(verifyUser.role)) {
-                throw new Error("You are not authorized!")
+                throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized!")
             }
 
             next();
